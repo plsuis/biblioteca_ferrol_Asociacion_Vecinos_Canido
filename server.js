3a7a8fdd6bbedb46038791dfa1fa1d750cee4662
suplis <israel.surribas.planas@gmail.com>
2024-08-29 12:15:58 +0200

    Agrego a instalación de cors e comparto a carpeta public

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(path.join(__dirname,"public")))
 // MIDDLEWARES
 
 // END-POINTS
