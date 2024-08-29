36d0867f8f94827a1c89521e35c3a0b159d74b35
suplis <israel.surribas.planas@gmail.com>
2024-08-29 18:34:38 +0200

    Isto é un novo comentario para a línea 20 > Probo o editor nano

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.static(path.join(__dirname,"public")))
 // END-POINTS
 
 // isto é unha proba para editar un commit
+// isto é outra mensaxe de comentario
 app.get(endpoints.xestion.biblioteca, paxinaBiblioteca); // biblioteca.html
 app.get(endpoints.xestion.prestamos, paxinaPrestamos); // prestamos.html
 app.get(endpoints.paxina.formularioLibro, paxinaNovoLibro); // novo-libro.html
