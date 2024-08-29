0fcab584d2dc93de03049313025134014a462a29
suplis <israel.surribas.planas@gmail.com>
2024-08-29 12:41:40 +0200

    Agrego carpeta datos
    Agrego o arquivo de varios, onde estou a poñer os endpoints iniciais

---
+++ datos/varios.js
@@ -0,0 +1,13 @@
+const endpoints = {
+    xestion: {
+        biblioteca: "/xestion-biblioteca",
+        prestamos:"/xestion-prestamos"
+    },
+    paxina:{
+        formularioLibro: "/engadir-novo-libro"
+    }
+}
+
+module.exports = {
+    endpoints
+}
