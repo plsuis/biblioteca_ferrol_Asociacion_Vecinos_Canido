1903cf03a5c9f00a3abe800c75ba5d14a702d6c5
sabelame <sabelame@gmail.com>
2024-09-06 11:02:06 +0200

    modifico e engado funcions para poder ter acceso a creacion de novos usuarios

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
-const { paxinaBiblioteca, paxinaPrestamos, paxinaNovoLibro, paxinaNovoPrestamo, paxinaBibliotecaSenUsuario, paxinaLogueoAdmin, paxinaInicioAdmin } = require("./middlewares/funcions.views");
+const { paxinaBiblioteca, paxinaPrestamos, paxinaNovoLibro, paxinaNovoPrestamo, paxinaBibliotecaSenUsuario, paxinaLogueoAdmin, paxinaInicioAdmin, paxinaNovoUsuario } = require("./middlewares/funcions.views");
 const { endpoints } = require("./datos/varios.js");
 
 const app = express();
@@ -31,6 +31,7 @@ app.get(endpoints.paxinas.bibliotecaSenUsuario, paxinaBibliotecaSenUsuario); // 
 app.get(endpoints.paxinas.logueo, paxinaLogueoAdmin); // login-admin.html
 app.get(endpoints.paxinas.inicioAdmin, paxinaInicioAdmin); // inicio-admin.html
 app.get(endpoints.xestion.bibliotecaAdmin, paxinaBiblioteca); // biblioteca-admin.html
+app.get(endpoints.xestion.usuarios, paxinaNovoUsuario); // crear-usuario.html
 app.get(endpoints.xestion.prestamos, paxinaPrestamos); // prestamos-admin.html
 app.get(endpoints.paxinas.formularioLibro, paxinaNovoLibro); // novo-libro.html
 app.get(endpoints.paxinas.formularioPrestamo, paxinaNovoPrestamo)//novo-prestamo.html
