dc90d61aa208af1a823d24032a96067271a70130
suplis <israel.surribas.planas@gmail.com>
2024-09-15 22:44:15 +0200

    Modificando a tabla de búsqueda de libros, agora saen os libros

--- public/JavaScript/funcions/buscar-libro.js
+++ public/JavaScript/funcions/buscar-libro.js
@@ -14,60 +14,63 @@ let botonBorrarBuscar = document.querySelector("#boton-borrar-busqueda");
 
 //----- FUNCION PARA BUSCAR UN LIBRO CONCRETO -----//
 inputBarraBusqueda.addEventListener("input", async() =>{
-
+  //tabla.childrenNodes.remove()
   let valorInput = inputBarraBusqueda.value;
   
   let textoTitulo = valorInput.trim().toLowerCase();
   console.log("Valor Input-->", textoTitulo);
   
   let datoRecibido = await comunicacionBuscar(`buscar-libros/?textoTitulo=${textoTitulo}`);
-  //console.log(datoRecibido.mensaxe)
+  //console.log("inputBarraBusqueda.addEventListener datoRecibido.mensaxe ",datoRecibido.mensaxe)
   let i = 0;
-  let datosLibros = datoRecibido.mensaxe[i];
-  console.log(datosLibros)
- if(datosLibros !== undefined){
+  let datosLibros = datoRecibido.mensaxe;
+  //console.log(datosLibros)
+  if(datosLibros !== undefined){
   
 
- let fila = document.createElement("tr");
- tabla.append(fila);
+      let fila = document.createElement("tr");
+      tabla.append(fila);
 
- let tdTitulo = document.createElement("th");
- tdTitulo.innerText = "Título";
- fila.append(tdTitulo);
- let tdAutor = document.createElement("th");
- tdAutor.innerText = "Autor";
- fila.append(tdAutor);
- let tdIsbn = document.createElement("th");
- tdIsbn.innerText = "ISBN";
- fila.append(tdIsbn);
- let tdEditorial = document.createElement("th");
- tdEditorial.innerText = "Editorial";
- fila.append(tdEditorial);
- let tdAno = document.createElement("th");
- tdAno.innerText = "Ano de Edición";
- fila.append(tdAno);
- let tdXenero = document.createElement("th");
- tdXenero.innerText = "Xénero";
- fila.append(tdXenero);
+      let tdTitulo = document.createElement("th");
+      tdTitulo.innerText = "Título";
+      fila.append(tdTitulo);
+      let tdAutor = document.createElement("th");
+      tdAutor.innerText = "Autor";
+      fila.append(tdAutor);
+      let tdIsbn = document.createElement("th");
+      tdIsbn.innerText = "ISBN";
+      fila.append(tdIsbn);
+      let tdEditorial = document.createElement("th");
+      tdEditorial.innerText = "Editorial";
+      fila.append(tdEditorial);
+      let tdAno = document.createElement("th");
+      tdAno.innerText = "Ano de Edición";
+      fila.append(tdAno);
+      let tdXenero = document.createElement("th");
+      tdXenero.innerText = "Xénero";
+      fila.append(tdXenero);
 
  
-  //console.log(datoRecibido.mensaxe.length)
+  console.log("datoRecibido.mensaxe antes",datoRecibido.mensaxe)
  
-     for (i = 0; i < datoRecibido.mensaxe.length; i++) {
+      for (let i = 0; i < datoRecibido.mensaxe.length; i++) {
+      
+        let fila = document.createElement("tr");
+        
+        for (let propiedade in datosLibros[i]) {
+          console.log(" let propiedade in datosLibros ",propiedade)
+          let celda = document.createElement("td");
+          let dato = JSON.stringify(datosLibros[i][propiedade]);
+          console.log("dato ",dato);
+          celda.innerHTML = dato.replace(/\"/g, "");
+          console.log("celda? ",celda)
+          fila.append(celda);  
+        }
+        console.log("fila ",fila)
+        tabla.append(fila);
+      }
     
-     let fila = document.createElement("tr");
-    tabla.append(fila);
-    for (let propiedade in datosLibros) {
-   
-    let celda = document.createElement("td");
-    let dato = JSON.stringify(datosLibros[propiedade]);
-    //console.log(datosLibros.Prestado_Libro_Prestado);
-    celda.innerHTML = dato.replace(/\"/g, "");
-    fila.append(celda); 
   }
-}
-    
-}
 })
 botonBuscar.addEventListener("click", async() =>{
 
@@ -77,7 +80,7 @@ botonBuscar.addEventListener("click", async() =>{
   console.log("Valor Input-->", textoTitulo);
   
   let datoRecibido = await comunicacionBuscar(`buscar-libros/?textoTitulo=${textoTitulo}`);
-  //console.log(datoRecibido.mensaxe)
+  console.log('botonBuscar.addEventListener ',datoRecibido.mensaxe)
   let i = 0;
   let datosLibros = datoRecibido.mensaxe[i];
   console.log(datosLibros)
